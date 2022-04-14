d214f5d608adfcb73bb88ecbca718044fafe43be
younhoso <thdbsgh3443@naver.com>
2022-04-14 19:11:34 +0900

    카카오 지도 js 코드 개선

--- studyroom-dev/static/js/mainMap.js
+++ studyroom-dev/static/js/mainMap.js
@@ -1,13 +1,13 @@
 (function() {
-  let initData= {
+  let initData = {
     coords: [{latitude: null, longitude: null}]
   }
   const mapContainer = document.getElementById("map"), // 지도를 표시할 div
-    mapOption = {
-      center: new kakao.maps.LatLng(37.56682, 126.97865), // 지도의 중심좌표
-      level: 5, // 지도의 확대 레벨
-      mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
-    };
+        mapOption = {
+          center: new kakao.maps.LatLng(37.56682, 126.97865), // 지도의 중심좌표
+          level: 5, // 지도의 확대 레벨
+          mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
+        };
 
   // 지도를 생성한다
   const map = new kakao.maps.Map(mapContainer, mapOption);
@@ -15,23 +15,13 @@
   // 지도에 마커와 인포윈도우(말풍선)를 표시하는 함수입니다
   function displayMarker(locPosition, message) {
     const { latitude, longitude } = locPosition;
+    initData.coords = [{latitude: latitude, longitude: longitude}]
     const position = new kakao.maps.LatLng(latitude, longitude); // 지도의 중심좌표
     // 마커를 생성합니다
     const marker = new kakao.maps.Marker({
       map: map,
       position: position, // 현재위치의 마커 입니다.!!
     });
-
-    // 인포윈도우(말풍선)를 생성합니다
-    const infowindow = new kakao.maps.InfoWindow({
-      removable: false,
-    });
-
-    // 인포윈도우(말풍선)를 마커위에 표시합니다
-    infowindow.open(map, marker);
-
-    // 지도 중심좌표를 접속위치로 변경합니다
-    map.setCenter(position);
 
     // 커스텀 오버레이를 생성합니다
     new kakao.maps.CustomOverlay({
@@ -40,7 +30,11 @@
       content: message,
       yAnchor: 1,
     });
-    initData.coords = [{latitude: latitude, longitude: longitude}]
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map)
+    // 지도 중심좌표를 접속위치로 변경합니다
+    map.setCenter(position);
   }
 
   // geolocation true 기능 //
@@ -66,7 +60,6 @@
   };
   // geolocation false 인경우 기능 //
 
-  const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
   const base_url = "/map";
   async function getData() {
     const res = await fetch(base_url);
@@ -84,25 +77,8 @@
     }
 
     // data.json있는 데이터 불러와 뿌려준다. //
-    db.datas.forEach((item, i) => {
+    db.datas.forEach((item) => {
       const { name, positions } = item;
-
-      // 마커 이미지의 이미지 크기 입니다
-      const imageSize = new kakao.maps.Size(24, 35);
-
-      // 마커 이미지를 생성합니다
-      const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-      // 마커를 생성합니다
-      const marker = new kakao.maps.Marker({
-        map: map, // 인포윈도우가 표시될 지도
-        position: new kakao.maps.LatLng(positions[0], positions[1]),
-        image: markerImage, // 마커 이미지
-      });
-
-      // 인포윈도우를 마커위에 표시합니다
-      // infowindow.open(map, marker);
-      marker.setMap(map);
 
       // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
       const temp_html = `<div class="customoverlay">
@@ -111,12 +87,15 @@
                         </a></div>`;
 
       // 커스텀 오버레이를 생성합니다
-      new kakao.maps.CustomOverlay({
+      const marker = new kakao.maps.CustomOverlay({
         map: map,
         position: new kakao.maps.LatLng(positions[0], positions[1]),
         content: temp_html,
         yAnchor: 1,
       });
+
+      // 마커가 지도 위에 표시되도록 설정합니다.
+      marker.setMap(map);
     });
     // data.json있는 데이터 불러와 뿌려준다. //
   };
@@ -124,7 +103,6 @@
   function showMap(e) {
     const self = $(e.currentTarget);
     const i = self.children('i');
-    const moveLatLon = new kakao.maps.LatLng(initData.coords[0].latitude, initData.coords[0].longitude);
     if(i.hasClass('ic-map')){
       self.addClass('on')
       i.removeClass();
@@ -133,8 +111,7 @@
       $('.card_wrap').hide()
       $('.map_wrap').show()
       map.relayout()
-      map.setCenter(moveLatLon);
-      map.setDraggable(true);
+      map.setCenter(new kakao.maps.LatLng(initData.coords[0].latitude, initData.coords[0].longitude));
     } else {
       self.removeClass('on')
       i.addClass('ic-map');
