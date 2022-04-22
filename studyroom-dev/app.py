2774d2e12ea7cc3f96aa9a9338897633dd63d3c8
hyung_guen <fnvkd5316@gmail.com>
2022-04-22 22:09:03 +0900

    ajax-form add 'id' in App.py

--- studyroom-dev/app.py
+++ studyroom-dev/app.py
@@ -16,10 +16,14 @@ def cafeForm():
 
 @app.route("/cafeform", methods=["POST"])
 def saveCafeData():
+
 # 카페 이미지
 # 카페 이미지 저장 구현은 메인화면에 띄우는것 까지하고!
 
 # 카페 정보
+    cafeList = list(db.cafe_test.find({}, {'_id': False}))
+
+    id   = len(cafeList) + 1 #카페 번호
     name = request.form['name'] #카페 이름
     desc = request.form['desc'] #카페 설명
 
@@ -32,11 +36,14 @@ def saveCafeData():
     detailAddrInfo   = request.form['detailAddrInfo']   # User가 적어준 상세주소
     zonecodeInfo     = request.form['zonecodeInfo']     # 우편번호
 
-# 주소의 경도,위도
-    longitude = request.form['longitude']  # 경도
+# 주소의 위도, 경도
     latitude  = request.form['latitude']  # 위도
+    longitude = request.form['longitude'] # 경도
+
+# 생성된 날짜정보 넣기.
 
     doc = {
+            "id":   id,
             "name": name,
             "desc": desc,
             "addressInfo": addressInfo,
@@ -46,8 +53,8 @@ def saveCafeData():
             "bname2Info": bname2Info,
             "detailAddrInfo": detailAddrInfo,
             "zonecodeInfo": zonecodeInfo,
-            "longitude": longitude,
-            "latitude": latitude
+            "latitude": latitude,
+            "longitude": longitude
     }
 
     db.cafe_test.insert_one(doc)
@@ -56,16 +63,18 @@ def saveCafeData():
 
 @app.route("/main", methods=["GET"])
 def main_get():
-    cafes = list(db.cafe_test.find({}, {'_id':False}))
+    cafes = list(db.cafe_test.find({}, {'_id': False}))
     cafeList = []
 
     for cafe in cafes:
+        id = cafe['id']
         region = cafe['sidoInfo'] + ' ' + cafe['sigunguInfo']
 
         doc = {
+                'id': id,
                 'name': cafe['name'],
-                'region': region,
-                'imgUrl': ''
+                'imgUrl': '',
+                'region': region
         }
 
         cafeList.append(doc)
@@ -73,72 +82,37 @@ def main_get():
     # list = [
     #         {
     #             "id": 1,
-    #             "title": "커피바인더리",
+    #             "name": "커피바인더리",
     #             "imgUrl": "../static/images/download.jpg",
     #             "region": "서울 강남구",
     #             "createdAt": 1591714800000,
     #             "updatedAt": 1591714800000
     #         },
-    #         {
-    #             "id": 2,
-    #             "title": "커피바인더리",
-    #             "imgUrl": "../static/images/download-1.jpg",
-    #             "region": "서울 강남구",
-    #             "createdAt": 1592406000000,
-    #             "updatedAt": 1592406000000
-    #         },
-    #         {
-    #             "id": 3,
-    #             "title": "커피바인더리",
-    #             "imgUrl": "../static/images/download-2.jpg",
-    #             "region": "서울 강남구",
-    #             "createdAt": 1591196400000,
-    #             "updatedAt": 1591196400000
-    #         },
-    #         {
-    #             "id": 4,
-    #             "title": "커피바인더리",
-    #             "imgUrl": "../static/images/download-3.jpg",
-    #             "region": "서울 강남구",
-    #             "createdAt": 1594738800000,
-    #             "updatedAt": 1594738800000
-    #         },
-    #         {
-    #             "id": 5,
-    #             "title": "커피바인더리",
-    #             "imgUrl": "../static/images/download-4.jpg",
-    #             "region": "서울 강남구",
-    #             "createdAt": 1592924400000,
-    #             "updatedAt": 1592924400000
-    #         }
-    #         ]
+
     return jsonify({'datas': cafeList})
 
 @app.route("/map", methods=["GET"])
 def map_get():
-    list = [
-            {
-                "id": "1",
-                "name": "스튜디오 르페리",
-                "positions": [37.559643, 127.0811217]
-            },
-            {
-                "id": "2",
-                "name": "푸쉬커피",
-                "positions": [37.5522944, 127.091274]
-            },
-            {
-                "id": "3",
-                "name": "안다즈 커피",
-                "positions": [37.5503218, 127.0895022]
-            },
-            {
-                "id": "4",
-                "name": "카페 러슬",
-                "positions": [37.5486102, 127.0855324]
-            }
-            ]
-    return jsonify({'datas': list})
+    cafes = list(db.cafe_test.find({}, {'_id': False}))
+    cafeList = []
+
+    for cafe in cafes:
+        doc = {
+                "id": cafe['id'],
+                "name": cafe['name'],
+                "positions": [cafe['latitude'], cafe['longitude']]
+        }
+
+        cafeList.append(doc)
+
+    # list = [
+    #         {
+    #             "id": "1",
+    #             "name": "스튜디오 르페리",
+    #             "positions": [37.559643, 127.0811217]
+    #         }
+    #         ]
+    return jsonify({'datas': cafeList})
 
 @app.route("/login")
 def login_form():
---
+++ studyroom-dev/static/js/common.js
@@ -0,0 +1,1 @@
+// <script src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=83114de23fe00162e1579d913d1396fe&libraries=services"></script>
